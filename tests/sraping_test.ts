e4ab1d9967b3e3f043a73bdc2a3f016c5d307f56
Thomas Mattone <thomasmattone.perso@gmail.com>
2025-07-27 04:25:39 +0200

    test: get pieces

--- tests/sraping_test.ts
+++ tests/sraping_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
-import { fetchGrid } from "../src/scraping.ts";
+import { fetchGrid, getPieces } from "../src/scraping.ts";
 import { minify } from "npm:html-minifier-terser";
 
 async function normalizeHtml(html: string): Promise<string> {
@@ -16,4 +16,13 @@ Deno.test("fetchGrid", async () => {
   const gridHtml = await fetchGrid(puzzleId);
 
   assertEquals(normalizeHtml(gridHtml), normalizeHtml(expectedHtml));
+});
+
+Deno.test("getPieces", async () => {
+  const url = new URL("./fixtures/puzzle_1_grid.html", import.meta.url);
+  const gridHtml = await Deno.readTextFile(url);
+
+  const pieces = getPieces(gridHtml)
+
+  assertEquals(pieces, "______SS__S_S__M_M__M___M________SS_");
 });
